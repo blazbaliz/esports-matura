5b205c3f017c596fab925163007be0a7d80f9d2a
Blaž Baliž <47872846+blazbaliz@users.noreply.github.com>
2019-10-23 13:11:51 +0200

    Update varnost.php

--- varnost.php
+++ varnost.php
@@ -13,7 +13,7 @@ if (isset($_POST['shrani_mail'])) {
 		$update_mail = mysqli_query($connect, $sql_update_mail) or die("nemorem");
 		header("location: varnost.php");
 	}
-	else { $error_mail = "Uporabniški račun z tem naslovom že obstaja <br>";}
+	else { $error_mail = "* Uporabniški račun z tem naslovom že obstaja <br>";}
 }
 
 
@@ -26,7 +26,7 @@ if(isset($_POST['shrani_geslo'])) {
 		$update_password = mysqli_query($connect, $sql_update_password);		
 	}
 	else {
-		$error_password = "Vneseni gesli se ne ujemata <br>";
+		$error_password = "* Vneseni gesli se ne ujemata <br>";
 	}
 }
 
@@ -36,95 +36,121 @@ if(isset($_POST['shrani_geslo'])) {
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<meta name="viewport" content="width=device widht, intial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	
 	
 </head>
-<body>
-<?php include "header.php" ?>
-<br>
-<div class="profil">
-<div class="sidebar">
-<table>
-	<tr>
-		<td>	
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<a href="profil.php">Osnovne informacije</a>
-		</td>
-	</tr>
-	<tr>
-		<td>
-			Varnost
-		</td>
-	</tr>
-	<tr>
-		<td>
-			<a href="denarnica.php">Denarnica	
-		</td>
-	</tr>
-</table>	
+<body class="index">
+
+<div class="header">
+	<?php include "header.php" ?>
 </div>
-<br>
+
+<div class="subheader">
+	<div class="notranji_subheader">
+		<ul class="navi" style="padding: 13px 240px 0 0">
+			<a href="profil.php"><li>Osnovne informacije</li></a>
+			<a href="varnost.php"><li>Varnost</li></a>
+			<a href="denarnica.php"><li>Denarnica</li></a>
+		</ul>	
+	</div>
+</div>
+<form action="varnost.php" method="post">
 <?php
 if (mysqli_num_rows($result) > 0) {
 	while ($row = mysqli_fetch_array($result)) {
 ?>
-<div class="varnost">
-Vaš e-mail naslov: <button class="spremeni_mail">Uredi</button> <br>
-<?php echo $row["mail"]; ?> <br>
-Vaše geslo: <button class="spremeni_geslo">Uredi</button><br> 
-******** <br>
-</div>
-<br> <br>
-<!-- Pojavno okno spremeni mail-->
-<div id="modal_mail" class="modal_mail">
-	<form action="varnost.php" method="post">
-	 <div class="modal_mail_content">
-	 	<div class="modal_mail_header">
-	 		Spremeni uporabniški e-mail naslov
-	 	</div>
-	 	<div class="modal_mail_bodi">
-	 	<?php echo $error_mail ?>
-	 	Vnesite vaš nov naslov:<br>
-	 		<input type="text" name="new_mail" required placeholder="<?php echo $row["mail"]; ?>"></input>
-	 	</div>
-	 	<div class="modal_mail_footer">
-	 		<input type="submit" name="shrani_mail" value="Shrani" ></input>
-	 	</div>
-	 	
-	 </div>
-	 </form>
+
+
+<div class="container">
+<br>
+<div class="card text-center" style="width: 650px; margin:0 auto;">
+  <div class="card-header">
+ <h4> Varnost </h4>
+  </div>
+  <div class="card-body">
+    <p class="card-text">
+    <table class="table table-striped table-bordered" style="width: 550px; margin:0 auto; border-radius: 3px;">
+    <tr>
+      <th style="border-width: 0px 0 3px 0; text-align: left; background-color: white;">Vaš e-poštni naslov: <span style="color: red; font-size: 15px;float: right"><?php echo $error_mail ?></span> </th> 
+    </tr>
+  <tbody>
+    <tr>
+      <td style="border-width: 3px 0 3px 0; text-align: center;"><?php echo $row["mail"]; ?> &nbsp <button class="btn btn-sm btn-primary " style="float: right;" data-toggle="modal" data-target="#exampleModal">Uredi</button> </td>
+    </tr>
+    <tr>
+      <th scope="col" style="border-width: 3px 0 3px 0; text-align: left;">Vaše geslo: <span style="color: red; font-size: 15px;float: right"><?php echo $error_password ?></span></td>
+    </tr>
+    <tr>
+      <td style="border-width: 3px 0 3px 0; text-align: center;"><?php echo "********" ?> &nbsp <button class="btn btn-primary btn-sm" style="float: right;" data-toggle="modal" data-target="#exampleModal1">Uredi</button></td>
+    </tr>	
+  </tbody>
+</table></p>
+  </div>
+  <div class="card-footer text-muted">
+  	<span style="font-size: 11px">Nikomur ne posredujte svojih osebnih podatkov. Za vaše izgube v primeru zlorabe ne odgovarjamo</span>
+  </div>
 </div>
 
-<br><br>
-<!-- Pojavno okno spremeni geslo-->
-<div id="modal_geslo" class="modal_geslo">
-	<form action="varnost.php" method="post">
-	 <div class="modal_geslo_content">
-	 	<div class="modal_geslo_header">
-	 	Spremeni geslo:
-	 	</div>
-	 	<div class="modal_geslo_bodi">
-	 	<?php echo $error_password ?>
-	 	Vnesite novo geslo:<br>
-	 	<input type="password" name="password1" required placeholder="********"></input><br>
-	 	Potrdite novo geslo:<br>
-	 	<input type="password" name="password2" required placeholder="********"></input>
-	 	</div>
-	 	<div class="modal_geslo_footer">
-	 	<input type="submit" name="shrani_geslo" value="Shrani"></input>
-	 	</div>
-	 	
-	 </div>
-	 </form>
 </div>
+
+<!-- Modal email-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered"  role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Spremeni e-poštni naslov:</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      Vnesite nov e-poštni naslov:<br>
+	 		<input type="text" class="form-control" style="text-align: center;" name="new_mail" required placeholder="<?php echo $row['mail']; ?>"></input>    
+	  </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Zapri</button>
+        <input type="submit" class="btn btn-primary" name="shrani_mail" value="Shrani"></input>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal geslo-->
+<div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered"  role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Spremeni uporabniško geslo:</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      Vnesite novo uporabniško geslo: <br>
+      	<input type="password" name="password1" required placeholder="********" class="form-control" style="text-align: center;" ></input>
+      <br>
+		  Potrdite novo uporabniško geslo:<br>
+<input type="password" name="password2" required placeholder="********" class="form-control" style="text-align: center;" ></input>
+	  </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Zapri</button>
+        <input type="submit" class="btn btn-primary" name="shrani_geslo" value="Shrani"></input>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php
 	}
 }
  ?>
+</form>
 
-</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
